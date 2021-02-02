4fb9d520fa5820b657e3876e65437629c0fef6c3
chavarino <32093884+chavarino@users.noreply.github.com>
2021-02-02 10:24:19 +0100

    Delete enums.js

--- src/interfaces/enums.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TiposAlgoritmo;
-(function (TiposAlgoritmo) {
-    TiposAlgoritmo[TiposAlgoritmo["SECUENCIADOR"] = 1] = "SECUENCIADOR";
-    TiposAlgoritmo[TiposAlgoritmo["FIBONACCI"] = 2] = "FIBONACCI";
-    TiposAlgoritmo[TiposAlgoritmo["FACTORIAL"] = 3] = "FACTORIAL";
-})(TiposAlgoritmo = exports.TiposAlgoritmo || (exports.TiposAlgoritmo = {}));
